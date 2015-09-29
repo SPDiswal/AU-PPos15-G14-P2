906a5a80e75f88d8a75698d1973e2b09725c79b1
Steffen Diswal <steffen.diswal@gmail.com>
2015-09-29 12:43:19 +0200

    Added Android files.

---
+++ PP15-G14-Project2/src/dk/au/cs/pp15g14project2/PositioningActivity.java
@@ -0,0 +1,15 @@
+package dk.au.cs.pp15g14project2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PositioningActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
