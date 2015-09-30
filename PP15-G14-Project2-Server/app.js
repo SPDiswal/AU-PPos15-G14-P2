64759187b7281d962ca1b4e790071cb4b02fd560
Steffen Diswal <steffen.diswal@gmail.com>
2015-09-30 13:22:42 +0200

    Finished implementing server module (though it may still need some tweaks here and there).

--- PP15-G14-Project2-Server/app.js
+++ PP15-G14-Project2-Server/app.js
@@ -1,31 +1,33 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require("./routes/index");
 
 var app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
+app.use("/", index);
 
 app.use(function (req, res, next)
 {
-    var err = new Error('Not Found');
+    var err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
-if (app.get('env') === 'development')
+if (app.get("env") === "development")
 {
     app.use(function (err, req, res, next)
     {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render("error", {
             message: err.message,
             error:   err
         });
@@ -35,7 +37,7 @@ if (app.get('env') === 'development')
 app.use(function (err, req, res, next)
 {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
         message: err.message,
         error:   {}
     });
--- PP15-G14-Project2-Server/routes/index.js
+++ PP15-G14-Project2-Server/routes/index.js
@@ -1,14 +1,92 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var file = require("fs");
 
-router.get('/', function (req, res, next)
+var logFilename = __dirname + "/../public/pp15-g14-project2.log";
+
+router.get("/", function (request, response, next)
 {
-    
+    file.readFile(logFilename, "utf8", function (error, data)
+    {
+        if (error)
+        {
+            throw error;
+        }
+        else
+        {
+            var lines = data.split("\n");
+            var output = kmlStart();
+            
+            for (var i in lines)
+            {
+                if (lines[i] !== "")
+                {
+                    output += kmlElement(JSON.parse(lines[i]));
+                }
+            }
+            
+            output += kmlEnd();
+            response.header("Content-Type", "text/xml").send(output);
+        }
+    });
 });
 
-router.post('/', function (req, res, next)
+router.post("/", function (request, response, next)
 {
+    var latitude = request.body.latitude;
+    var longitude = request.body.longitude;
+    var altitude = request.body.altitude;
+    var time = request.body.time;
     
+    if (!latitude || !longitude || !altitude || !time)
+    {
+        response.sendStatus(400);
+    }
+    else
+    {
+        var output = {
+            latitude:  latitude,
+            longitude: longitude,
+            altitude:  altitude,
+            time:      time
+        };
+        
+        file.appendFile(logFilename, JSON.stringify(output) + "\n", function ()
+        {
+            console.log("Wrote to " + __dirname + "/public/pp15-g14-project2.log");
+            
+            response.sendStatus(204);
+        });
+    }
 });
+
+function kmlStart()
+{
+    return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+           "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n";
+}
+
+function kmlElement(location)
+{
+    return "\t<Placemark>\n" +
+           "\t<name>Simple placemark</name>\n" + // TODO
+           "\t<description>An interesting location</description>\n" + // TODO
+           "\t<Point>\n" +
+           "\t\t<coordinates>" +
+           location.latitude + "," + location.longitude + "," + location.altitude +
+           "</coordinates>\n" +
+           "\t</Point>\n" +
+           "\t<TimeStamp>\n" +
+           "\t\t<when>" +
+           location.time +
+           "</when>\n" +
+           "\t</TimeStamp>\n" +
+           "\t</Placemark>\n";
+}
+
+function kmlEnd()
+{
+    return "</kml>";
+}
 
 module.exports = router;
