54779bb2e81a79679c93f9cdc38f009b4a4924d4
ThomasLange <thomashlange@gmail.com>
2015-09-30 11:45:31 +0200

    Added stopListeningForUpdates to DistanceReporter

--- PP15-G14-Project2/src/dk/au/cs/pp15g14project2/reporters/DistanceReporter.java
+++ PP15-G14-Project2/src/dk/au/cs/pp15g14project2/reporters/DistanceReporter.java
@@ -12,32 +12,43 @@ public class DistanceReporter implements Reporter
     private static final String GPS = LocationManager.GPS_PROVIDER;
     
     private final int distanceInterval;
-    
-    public DistanceReporter(final int distanceInterval /* metres */)
+    private final LocationManager locationManager;
+    private final LocationListener listener;
+
+    public DistanceReporter(final LocationManager locationManager,
+                            final Logger logger,
+                            final int distanceInterval /* metres */)
     {
         this.distanceInterval = distanceInterval;
-    }
-    
-    public void listenForUpdates(final LocationManager locationManager, final Logger logger)
-    {
-        locationManager.requestLocationUpdates(GPS, FIXED_TIME_INTERVAL, distanceInterval, new LocationListener()
+        this.locationManager = locationManager;
+        this.listener = new LocationListener()
         {
             public void onLocationChanged(final Location location)
             {
                 logger.log(TAG, LocationPrinter.convertToString(location));
             }
-            
+
             public void onStatusChanged(final String provider, final int status, final Bundle extras)
             {
             }
-            
+
             public void onProviderEnabled(final String provider)
             {
             }
-            
+
             public void onProviderDisabled(final String provider)
             {
             }
-        });
+        };
+    }
+
+    public void listenForUpdates()
+    {
+        locationManager.requestLocationUpdates(GPS, FIXED_TIME_INTERVAL, distanceInterval, listener);
+    }
+
+    @Override
+    public void stopListeningForUpdates() {
+        locationManager.removeUpdates(listener);
     }
 }
