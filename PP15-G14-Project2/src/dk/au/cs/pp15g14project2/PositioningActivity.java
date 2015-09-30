556d6813eb17b9777770ad714d217e2f49dca49c
Steffen Diswal <steffen.diswal@gmail.com>
2015-09-30 11:46:52 +0200

    Adapted positioning activity to new reporting interface.

--- PP15-G14-Project2/src/dk/au/cs/pp15g14project2/PositioningActivity.java
+++ PP15-G14-Project2/src/dk/au/cs/pp15g14project2/PositioningActivity.java
@@ -15,6 +15,7 @@ public class PositioningActivity extends Activity
     private Reporter reporter;
     private LocationManager locationManager;
     private SensorManager sensorManager;
+    private CompositeLogger logger;
     
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -23,12 +24,17 @@ public class PositioningActivity extends Activity
         
         this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         this.sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+    
+        this.logger = new CompositeLogger();
+        this.logger.add(new ConsoleLogger());
+        this.logger.add(new FileLogger());
         
         setContentView(R.layout.main);
     }
     
     public void onChoosingReporter(View view)
     {
+        if (reporter != null) reporter.stopListeningForUpdates();
         boolean isChecked = ((RadioButton) view).isChecked();
         
         switch (view.getId())
@@ -37,7 +43,7 @@ public class PositioningActivity extends Activity
                 if (isChecked)
                 {
                     // TODO Allow variable time interval.
-                    reporter = new TimeReporter(5);
+                    reporter = new TimeReporter(locationManager, logger, 5);
                     Toast.makeText(this, "Using time reporting", Toast.LENGTH_LONG).show();
                 }
                 break;
@@ -46,7 +52,7 @@ public class PositioningActivity extends Activity
                 if (isChecked)
                 {
                     // TODO Allow variable distance interval.
-                    reporter = new DistanceReporter(10);
+                    reporter = new DistanceReporter(locationManager, logger, 10);
                     Toast.makeText(this, "Using distance reporting", Toast.LENGTH_LONG).show();
                 }
                 break;
@@ -55,7 +61,7 @@ public class PositioningActivity extends Activity
                 if (isChecked)
                 {
                     // TODO Allow variable distance interval.
-                    reporter = new SpeedReporter(10, 2);
+                    reporter = new SpeedReporter(locationManager, logger, 10, 2);
                     Toast.makeText(this, "Using speed reporting", Toast.LENGTH_LONG).show();
                 }
                 break;
@@ -64,16 +70,12 @@ public class PositioningActivity extends Activity
                 if (isChecked)
                 {
                     // TODO Allow variable distance interval.
-                    reporter = new MotionReporter(sensorManager, 10);
+                    reporter = new MotionReporter(sensorManager, locationManager, logger, 10);
                     Toast.makeText(this, "Using motion reporting", Toast.LENGTH_LONG).show();
                 }
                 break;
         }
-        
-        CompositeLogger logger = new CompositeLogger();
-        logger.add(new ConsoleLogger());
-        logger.add(new FileLogger());
-        
-        reporter.listenForUpdates(locationManager, logger);
+    
+        if (reporter != null) reporter.listenForUpdates();
     }
 }
