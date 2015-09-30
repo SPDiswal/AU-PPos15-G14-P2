61b2b3e10f3e688a5db958c9fe1095e983f1f35b
Steffen Diswal <steffen.diswal@gmail.com>
2015-09-30 12:14:56 +0200

    Added server module in node.js

---
+++ PP15-G14-Project2-Server/app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use(function (req, res, next)
+{
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development')
+{
+    app.use(function (err, req, res, next)
+    {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error:   err
+        });
+    });
+}
+
+app.use(function (err, req, res, next)
+{
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error:   {}
+    });
+});
+
+module.exports = app;
---
+++ PP15-G14-Project2-Server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next)
+{
+    
+});
+
+router.post('/', function (req, res, next)
+{
+    
+});
+
+module.exports = router;
